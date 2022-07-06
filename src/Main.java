71548927cfe931003f9d1486d795cd5448fbc863
Kirill <uspenskiyke@mail.ru>
2022-07-06 16:53:37 +0400

    Первый коммит

---
+++ src/Main.java
@@ -0,0 +1,22 @@
+
+
+
+
+public class Main {
+    public static void main(String[] args) {
+
+    }
+
+    public static boolean Register(String login, String password, String confirmPassword) {
+
+
+    }
+
+    public static String exceptionCatch(String login, String password, String confirmPassword) {
+
+
+    }
+}
+
+
+
